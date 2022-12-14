a272d52263ba5530de243ada00997e00d5857757
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-12-14 08:45:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let newName = [];
+    for(let i=0; i<name.length; i++) {
+        let message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        newName.push(message);
+    }
+    return newName
+}
+function countDown(num){
+    for(let i=num ; i>=0; i--){
+        console.log(i);
+    }
+}
+console.log(countDown(10));
